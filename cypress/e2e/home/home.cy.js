d48b30f2b228ec364c954e95a9fda6f8056c5a02
Alexandre Lunkes <lunkes.alexandre@gmail.com>
2023-05-18 22:56:08 -0300

    teste

--- cypress/e2e/home/home.cy.js
+++ cypress/e2e/home/home.cy.js
@@ -3,7 +3,7 @@
 describe('página home',()=>{
     it('',()=>{
         cy.visit('https://area-de-teste.vercel.app/')
-            .get('#titulo-home')
+            .get('#titulo-home aaasdasd')
             .should('have.text','Seja bem vindo à Área de Testes')
     })
 })
